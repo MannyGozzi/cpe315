6ee03e7691b0b3e55bfafaad531b616edc62bf6f
Noctua <mannygozzidev@protonmail.com>
2023-01-25 17:41:56 -0800

    Added opcodes

--- 2AssemblyToBinary/src/Instruction.java
+++ 2AssemblyToBinary/src/Instruction.java
@@ -5,9 +5,24 @@ A predefined mapping for the MIPS instruction set
  */
 public class Instruction {
     Map<String, String> type;
+    Map<String, String> format;
 
     Instruction() {
         type = new TreeMap<>();
-        type.put("add", "");
+        format = new TreeMap<>();
+        type.put("and",     "100100"); format.put("and",    "R");
+        type.put("or",      "100101"); format.put("or",     "R");
+        type.put("add",     "100000"); format.put("add",    "R");
+        type.put("addi",    "001000"); format.put("addi",   "R");
+        type.put("sll",     "000000"); format.put("sll",    "R");
+        type.put("sub",     "100010"); format.put("sub",    "R");
+        type.put("slt",     "101010"); format.put("slt",    "R");
+        type.put("beq",     "000100"); format.put("beq",    "R");
+        type.put("bne",     "000101"); format.put("bne",    "R");
+        type.put("lw",      "100011"); format.put("lw",     "I");
+        type.put("sw",      "101011"); format.put("sw",     "I");
+        type.put("j",       "000010"); format.put("j",      "J");
+        type.put("jr",      "001000"); format.put("jr",     "J");
+        type.put("jal",     "000011"); format.put("jal",    "J");
     }
 }
--- 2AssemblyToBinary/src/MIPSParser.java
+++ 2AssemblyToBinary/src/MIPSParser.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /*
 Resource https://www.dcc.fc.up.pt/~ricroc/aulas/1920/ac/apontamentos/P04_encoding_mips_instructions.pdf
  */
@@ -15,8 +16,8 @@ public class MIPSParser {
         this.inputFile = inputFile;
         labelToBinLoc = new TreeMap<>();
         firstPass();
+        printMapping();
         secondPass();
-
     }
 
     /*
@@ -25,10 +26,27 @@ public class MIPSParser {
     private void firstPass() {
         try {
             BufferedReader asm = new BufferedReader(new FileReader(inputFile));
+            int counter = 0;
             while (asm.ready()) {
-                String line = asm.readLine();
-
-                System.out.println(line);
+                String line = asm.readLine().trim();
+                if (line.length() == 0 || line.charAt(0) == '#') continue;
+                Pattern labelPattern = Pattern.compile("(\\w+):(.*)", Pattern.CASE_INSENSITIVE);
+                Matcher matcher = labelPattern.matcher(line);
+                if (matcher.find()) {
+                    System.out.println("match " + counter + ": " + matcher.group(1) + " remainder: \"" + matcher.group(2) + "\""); // gets the first match
+//                    if (matcher.group(2).length() > 0) {
+//                        labelToBinLoc.put(matcher.group(1), Integer.toBinaryString(counter));
+//                    }
+                    labelToBinLoc.put(matcher.group(1), Operations.getBinaryWithSize(counter, 26));
+//                    else {
+//                        while(asm.ready()) {
+//                            line = asm.readLine().trim();
+//                            if (line.length() != 0 && line.charAt(0) != '#') break;
+//                        }
+//                        labelToBinLoc.put(matcher.group(1), Integer.toBinaryString(counter));
+//                    }
+                }
+                ++counter;
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -40,13 +58,44 @@ public class MIPSParser {
     private void secondPass() {
         try {
             BufferedReader asm = new BufferedReader(new FileReader(inputFile));
+            int counter = 0;
             while (asm.ready()) {
-                String line = asm.readLine();
-
-                System.out.println(line);
+                String line = asm.readLine().trim();
+                if (line.length() == 0 || line.charAt(0) == '#') continue;
+                Pattern labelPattern = Pattern.compile("(\\w+):(.*)", Pattern.CASE_INSENSITIVE);
+                Matcher matcher = labelPattern.matcher(line);
+                if (matcher.find()) {
+                    if (matcher.group(2).length() > 0) {
+                        processCommand(matcher.group(2).trim());
+                    }
+                    else {
+                        while(asm.ready()) {
+                            line = asm.readLine().trim();
+                            if (line.length() != 0 && line.charAt(0) != '#') break;
+                        }
+                        processCommand(line.trim());
+                    }
+                }
+                ++counter;
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void processCommand(String command) {
+        if (command.charAt(0) == '#') return;
+
+        Pattern labelPattern = Pattern.compile("(\\w+):(.*)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = labelPattern.matcher(command);
+        if (matcher.find()) {
+            System.out.println("match: " + matcher.group(1) + " remainder: \"" + matcher.group(2) + "\""); // gets the first match
+        }
+    }
+
+    private void printMapping() {
+        for (String label : labelToBinLoc.keySet()) {
+            System.out.println(label + ":\t " + labelToBinLoc.get(label));
         }
     }
 }
