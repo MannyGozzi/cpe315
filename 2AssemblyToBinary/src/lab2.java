068a24679908c71db57f8142a57f54b7af50743b
Manuel Gozzi <manuelgozzi@Gozzi-M1.local>
2023-01-27 09:55:43 -0800

    Fixed main file to lab2

--- 2AssemblyToBinary/src/lab2.java
+++ 2AssemblyToBinary/src/lab2.java
@@ -1,10 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Map;
-import java.util.TreeMap;
-
-public class Main {
+public class lab2 {
     public static void main(String[] args) {
         String inputFile = args[0];
         if (inputFile == null) {
