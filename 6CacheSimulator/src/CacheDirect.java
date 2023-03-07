0eef66d1a8a3f83e56450a2a5b4763185476219a
Noctua <mannygozzidev@protonmail.com>
2023-03-06 23:00:30 -0800

    Started project

--- 6CacheSimulator/src/CacheDirect.java
+++ 6CacheSimulator/src/CacheDirect.java
@@ -2,27 +2,28 @@ public class CacheDirect {
     private int sizeTags;
     private int sizeWordBlock;
     private int[] cache;
+    private int[] tags;
     public CacheDirect(int sizeKB, int wordBlock) {
-        sizeTags = sizeKB * 1024 / wordBlock;
+        sizeTags = sizeKB * 1024 / 32 / wordBlock;
         sizeWordBlock = wordBlock;
         cache = new int[sizeTags];
+        tags = new int[sizeTags];
+
+        for (int i = 0; i < sizeTags; i++) {
+            cache[i] = -1;
+            tags[i] = -1;
+        }
     }
     /* returns true if the address was a hit, false otherwise
     * Will automatically add the corresponding values to the cache */
     public boolean read(int address) {
-        int tag = address / sizeWordBlock;
-        if (cache[tag] == 0) {
+        int tag = address % sizeTags;
+        int targetTag = address / sizeTags;
+        if (tags[tag] != targetTag || cache[tag] == 0) {
             cache[tag] = 1;
+            tags[tag] = targetTag;
             return false;
         }
         return true;
     }
-
-    public void printCache() {
-        for (int i = 0; i < sizeTags; i++) {
-            System.out.println("Tag " + i + ": " + cache[i]);
-        }
-    }
-
-
 }
--- 6CacheSimulator/src/Main.java
+++ 6CacheSimulator/src/Main.java
@@ -7,16 +7,29 @@ public class Main {
             System.exit(1);
         }
         try {
-            File file = new File("input.txt");
+            String inputFilename = args[0];
+            File file = new File(inputFilename);
             Scanner input = new Scanner(file);
             CacheDirect cache = new CacheDirect(2, 1);
+            int hits = 0; int misses = 0;
             while (input.hasNext()) {
                 String line = input.nextLine();
-
+                int address = Integer.parseInt(line, 16);
+                if (cache.read(address)) {
+                    ++hits;
+                } else {
+                    ++misses;
+                }
             }
-
+            printStats(hits, misses, 1, 2, 1, 1);
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    private static void printStats(int hits, int misses, int cacheNum, int cacheSizeKB, int associativity, int blockSize) {
+        System.out.println("Cache #" + cacheNum);
+        System.out.println("Cache Size: " + cacheSizeKB*1024 + "B\t" + "Associativity: " + associativity + "\t" + "Block Size: " + blockSize);
+        System.out.print("Hits: " + hits + "\t"); System.out.format("Hit Rate: %.2f%%", (double) hits/(hits+misses) * 100);
     }
 }
